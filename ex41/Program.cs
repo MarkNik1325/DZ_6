caf5fd4ae70384dec9a6653a734c29dc9fffa991
*Nikolai* <markyshinikolai@icloud.com>
2023-02-20 00:55:29 +0300

    DZ_6

---
+++ ex41/Program.cs
@@ -0,0 +1,39 @@
+﻿int ReadInt (string argumentName)
+{
+Console.WriteLine($"Введите колличество чисел в массиве {argumentName}");
+  int number = 0;
+
+ while (!int.TryParse (Console.ReadLine(), out number))
+ {
+    Console.WriteLine("Its not number");
+ }
+ return number;
+}
+
+void RandomArray (int[] array)
+ {
+    Random random = new Random();
+    for (int i =0; i < array.Length; i++)
+    {
+        array [i] = random.Next(-10, 11);
+    }
+ }
+
+int FindPositive (int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (array[i] >0)
+        {
+        sum++;
+        }
+    }
+    return sum;
+}
+
+int number = ReadInt ("");
+int [] array = new  int[number];
+RandomArray (array);
+Console.WriteLine (string.Join(" ", array));
+Console.WriteLine ($"Колличество чисел больше нуля равняется {FindPositive (array)}");
---
+++ ex43/Program.cs
@@ -0,0 +1,50 @@
+﻿
+double ReadInt (string argumentName)
+{
+Console.WriteLine($"Введите значение {argumentName}");
+  int number = 0;
+
+ while (!int.TryParse (Console.ReadLine(), out number))
+ {
+    Console.WriteLine("Its not number");
+ }
+ return number;
+}
+
+double FindCoordinateX (double k1, double k2, double b1, double b2)
+{
+double coordx = (b2 - b1)/(k1-k2);
+return coordx;
+}
+
+double FindCoordinateY (double k1, double k2, double b1, double b2)
+{
+double coordy = (k1*b2 - k2*b1)/(k1-k2);
+return coordy;
+}
+
+bool Validate (double k1, double k2, double b1, double b2)
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2)
+        {
+        Console.WriteLine("Прямые совпадают"); 
+        return false;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Прямые параллельны"); 
+        return false;
+    }
+    return true;
+}
+
+
+double k1 = ReadInt ("k1");
+double k2 = ReadInt ("k2");
+double b1 = ReadInt ("b1");
+double b2 = ReadInt ("b2");
+Validate(k1, k2, b1, b2);
+Console.WriteLine ($"Точки пересечения прямых y = {FindCoordinateY (k1, k2, b1, b2)}, x = {FindCoordinateX(k1, k2, b1, b2)}");
